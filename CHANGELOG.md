5ec1b1aaaa4a24ecaf6501ccc80e95f0a05059bc
David Barragán Merino <david.barragan@kaleidos.net>
2015-05-05 19:17:52 +0200

    Make plugin compatible only with taiga 1.7 release

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog #
+
+
+## 0.0.2
+
+- Add compatibility with taiga >= 1.7.
+
+
+## 0.0.1
+
+- Initial release.
--- back/taiga_contrib_letschat/serializers.py
+++ back/taiga_contrib_letschat/serializers.py
@@ -14,7 +14,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from .compat import serializers
+from taiga.base.api import serializers
+
 from .models import LetsChatHook
 
 
